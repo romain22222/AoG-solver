cb8d32869f98ae0a858852457b1c8e83f85c6be6
romain22222 <romrom22222@hotmail.fr>
2026-04-01 12:11:58 +0200

    small match optimization

--- constraints/match.py
+++ constraints/match.py
@@ -6,13 +6,21 @@ class MatchConstraint(Constraint):
     def __init__(self):
         super().__init__()
         self.reference_shape = None
+        self.allowed_sizes = []
     
     def propagate(self, state) -> bool:
+        if len(self.allowed_sizes) == 0:
+            amCells = len(state.grid.cells)
+            for i in range(amCells):
+                if amCells % (i + 1) == 0:
+                    self.allowed_sizes.append(i + 1)
         if self.reference_shape is None:
             for region_id in state.uf.parentList:
                 cells = get_region_cells(state, region_id)
                 if len(state.uf.connectables[region_id]) == 0:
                     self.reference_shape = get_region_shape(cells)
+                    if len(self.reference_shape) not in self.allowed_sizes:
+                        return False
                     break
             else:
                 return True
